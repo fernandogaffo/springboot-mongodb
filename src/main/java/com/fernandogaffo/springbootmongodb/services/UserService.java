a7288bf756c6781304035e5bd94c8c384a292a75
Fernando Gaffo <fernandogaffo@gmail.com>
2021-03-18 18:33:27 -0300

    Obter usuário por ID

--- src/main/java/com/fernandogaffo/springbootmongodb/services/UserService.java
+++ src/main/java/com/fernandogaffo/springbootmongodb/services/UserService.java
@@ -1,12 +1,14 @@
 package com.fernandogaffo.springbootmongodb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.fernandogaffo.springbootmongodb.domain.User;
 import com.fernandogaffo.springbootmongodb.repository.UserRepository;
+import com.fernandogaffo.springbootmongodb.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+
+	public User findById(String id) {
+		Optional<User> user = repo.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
