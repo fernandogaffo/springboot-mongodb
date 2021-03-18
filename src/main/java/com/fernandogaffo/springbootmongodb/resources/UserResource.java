289fb2a6161d36030b40d868436fbe808aa90bd1
Fernando Gaffo <fernandogaffo@gmail.com>
2021-03-18 18:19:51 -0300

    DTO para Retornar Usuário

--- src/main/java/com/fernandogaffo/springbootmongodb/resources/UserResource.java
+++ src/main/java/com/fernandogaffo/springbootmongodb/resources/UserResource.java
@@ -1,8 +1,7 @@
 package com.fernandogaffo.springbootmongodb.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,18 +10,20 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fernandogaffo.springbootmongodb.domain.User;
+import com.fernandogaffo.springbootmongodb.dto.UserDTO;
 import com.fernandogaffo.springbootmongodb.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
-	
+
 	@Autowired
 	private UserService service;
-	
+
 	@RequestMapping(method = RequestMethod.GET)
-	public ResponseEntity<List<User>> findAll() {
+	public ResponseEntity<List<UserDTO>> findAll() {
 		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 }
