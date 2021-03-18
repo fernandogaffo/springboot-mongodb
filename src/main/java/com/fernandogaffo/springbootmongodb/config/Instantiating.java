7c7031d168549eb77674e0c60488ce4261ad39dc
Fernando Gaffo <fernandogaffo@gmail.com>
2021-03-18 18:14:43 -0300

    Instanciação da Base de Dados

---
+++ src/main/java/com/fernandogaffo/springbootmongodb/config/Instantiating.java
@@ -0,0 +1,29 @@
+package com.fernandogaffo.springbootmongodb.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.fernandogaffo.springbootmongodb.domain.User;
+import com.fernandogaffo.springbootmongodb.repository.UserRepository;
+
+@Configuration
+public class Instantiating implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		userRepository.deleteAll();
+
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+	}
+
+}
