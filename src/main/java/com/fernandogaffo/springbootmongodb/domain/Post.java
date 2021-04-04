3af6a7c7bf75fff1ce30757b72c3417f21f4512d
Fernando Gaffo <fernandogaffo@gmail.com>
2021-04-04 10:49:49 -0300

    Referenciando os posts do usuário

--- src/main/java/com/fernandogaffo/springbootmongodb/domain/Post.java
+++ src/main/java/com/fernandogaffo/springbootmongodb/domain/Post.java
@@ -8,7 +8,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.fernandogaffo.springbootmongodb.dto.AuthorDTO;
 
-@Document
+@Document(collection = "post")
 public class Post implements Serializable {
 	private static final long serialVersionUID = 1L;
 
