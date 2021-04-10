1b9dd6956ad487290af359dcd884d7f87bdd5c74
Fernando Gaffo <fernandogaffo@gmail.com>
2021-04-09 21:05:04 -0300

    Consulta com vários critérios

--- src/main/java/com/fernandogaffo/springbootmongodb/services/PostService.java
+++ src/main/java/com/fernandogaffo/springbootmongodb/services/PostService.java
@@ -1,5 +1,6 @@
 package com.fernandogaffo.springbootmongodb.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -18,11 +19,16 @@ public class PostService {
 
 	public Post findById(String id) {
 		Optional<Post> post = repo.findById(id);
-		return post.orElseThrow(() 	-> new ObjectNotFoundException("Objeto não encontrado"));
+		return post.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
-	
+
 	public List<Post> findByTitle(String text) {
-		//return repo.findByTitleContainingIgnoreCase(text);
+		// return repo.findByTitleContainingIgnoreCase(text);
 		return repo.searchTitle(text);
+	}
+
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+		return repo.fullSearch(text, minDate, maxDate);
 	}
 }
